1a7bf8d90bfb2e7ef1e531e50b5681bf55255768
narayan1989-bais <narayan.bhagwan@trianz.com>
2025-09-24 19:56:18 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
+set -x
+set -e
 
-WORKDIR=/home/ec2-user/sample.daytrader7
+REPO_DIR="/home/ec2-user/sample.daytrader7"
+REPO_URL="https://github.com/narayan1989-bais/sample.daytrader7.git"
 
-# Remove existing folder if it exists
-if [ -d "$WORKDIR" ]; then
-  echo "Removing existing directory $WORKDIR"
-  rm -rf "$WORKDIR"
+# Remove existing directory if it exists
+if [ -d "$REPO_DIR" ]; then
+  echo "Removing existing directory $REPO_DIR"
+  rm -rf "$REPO_DIR"
 fi
 
 # Clone fresh repo
 echo "Cloning repository"
-git clone https://github.com/narayan1989-bais/sample.daytrader7.git "$WORKDIR"
+git clone "$REPO_URL" "$REPO_DIR"
 
-cd "$WORKDIR"
-
+# Run Maven install
 echo "Running Maven install"
-#mvn clean install
+cd "$REPO_DIR"
+#mvn install
 
 # Kill any process on port 9082
 PORT=9082
 PID=$(lsof -ti tcp:$PORT)
 if [ ! -z "$PID" ]; then
-  echo "Killing PID $PID"
+  echo "Killing process on port $PORT with PID $PID"
   kill -9 $PID
   sleep 2
 fi
 
+# Start Liberty server
 echo "Starting Liberty server"
 cd daytrader-ee7
 nohup mvn liberty:run > liberty.log 2>&1 &

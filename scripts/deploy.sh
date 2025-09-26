889916ed07587499bc0f362bd2dcaa350e56b776
narayan1989-bais <narayan.bhagwan@trianz.com>
2025-09-26 14:11:40 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,20 +18,20 @@ if [ $? -ne 0 ]; then
 fi
 echo "Cloned repo" >> $LOGFILE
 
-# Build entire project from root as ec2-user
+# Build entire project (runs mvn install on root)
 cd /home/ec2-user/sample.daytrader7 || exit 1
-sudo -u ec2-user mvn clean install >> $LOGFILE 2>&1
+sudo -u ec2-user mvn install >> $LOGFILE 2>&1
 if [ $? -ne 0 ]; then
-  echo "Build failed!" >> $LOGFILE
+  echo "Maven install failed!" >> $LOGFILE
   exit 1
 fi
-echo "Build succeeded" >> $LOGFILE
+echo "Maven install succeeded" >> $LOGFILE
 
-# Kill any running Liberty server started by previous deployments
+# Kill any running Liberty server
 sudo -u ec2-user pkill -f "mvn liberty:run" || true
 echo "Killed any existing Liberty server" >> $LOGFILE
 
-# Start Liberty server in background as ec2-user inside daytrader-ee7
+# Start Liberty server in daytrader-ee7 folder
 cd /home/ec2-user/sample.daytrader7/daytrader-ee7 || exit 1
 sudo -u ec2-user nohup mvn liberty:run > $LIBERTYLOG 2>&1 &
 echo "Liberty server started" >> $LOGFILE

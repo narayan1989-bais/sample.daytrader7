568e3188eff405299b3d3f21d458407c42bb2ba0
narayan1989-bais <narayan.bhagwan@trianz.com>
2025-09-26 14:04:51 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-echo "Starting deploy script..." > /home/ec2-user/deploy.log
-date >> /home/ec2-user/deploy.log
+LOGFILE=/home/ec2-user/deploy.log
+LIBERTYLOG=/home/ec2-user/liberty.log
+
+echo "Starting deploy script..." > $LOGFILE
+date >> $LOGFILE
 
 # Remove old repo
 rm -rf /home/ec2-user/sample.daytrader7
-echo "Removed old repo" >> /home/ec2-user/deploy.log
+echo "Removed old repo" >> $LOGFILE
 
-# Clone fresh repo
-git clone https://github.com/narayan1989-bais/sample.daytrader7.git /home/ec2-user/sample.daytrader7
-echo "Cloned repo" >> /home/ec2-user/deploy.log
+# Clone fresh repo as ec2-user
+sudo -u ec2-user git clone https://github.com/narayan1989-bais/sample.daytrader7.git /home/ec2-user/sample.daytrader7
+if [ $? -ne 0 ]; then
+  echo "Git clone failed!" >> $LOGFILE
+  exit 1
+fi
+echo "Cloned repo" >> $LOGFILE
 
-# Build project
-cd /home/ec2-user/sample.daytrader7
-mvn clean install >> /home/ec2-user/deploy.log 2>&1
+# Build entire project from root as ec2-user
+cd /home/ec2-user/sample.daytrader7 || exit 1
+sudo -u ec2-user mvn clean install >> $LOGFILE 2>&1
+if [ $? -ne 0 ]; then
+  echo "Build failed!" >> $LOGFILE
+  exit 1
+fi
+echo "Build succeeded" >> $LOGFILE
 
-## Start Liberty server
-cd daytrader-ee7
-nohup mvn liberty:run > /home/ec2-user/liberty.log 2>&1 &
+# Kill any running Liberty server started by previous deployments
+sudo -u ec2-user pkill -f "mvn liberty:run" || true
+echo "Killed any existing Liberty server" >> $LOGFILE
 
-echo "Liberty started" >> /home/ec2-user/deploy.log
+# Start Liberty server in background as ec2-user inside daytrader-ee7
+cd /home/ec2-user/sample.daytrader7/daytrader-ee7 || exit 1
+sudo -u ec2-user nohup mvn liberty:run > $LIBERTYLOG 2>&1 &
+echo "Liberty server started" >> $LOGFILE

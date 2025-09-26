23fea3b004bcb260d5a746e3c31273f6c51a1606
narayan1989-bais <narayan.bhagwan@trianz.com>
2025-09-26 14:37:25 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,37 +1,24 @@
 #!/bin/bash
 
-LOGFILE=/home/ec2-user/deploy.log
-LIBERTYLOG=/home/ec2-user/liberty.log
+LIBERTYLOG=liberty.log
 
-echo "Starting deploy script..." > $LOGFILE
-date >> $LOGFILE
-
-# Remove old repo
-rm -rf /home/ec2-user/sample.daytrader7
-echo "Removed old repo" >> $LOGFILE
-
-# Clone fresh repo as ec2-user
-sudo -u ec2-user git clone https://github.com/narayan1989-bais/sample.daytrader7.git /home/ec2-user/sample.daytrader7
+echo "Cloning repo"
+git clone https://github.com/narayan1989-bais/sample.daytrader7.git
 if [ $? -ne 0 ]; then
-  echo "Git clone failed!" >> $LOGFILE
+  echo "Git clone failed!"
   exit 1
 fi
-echo "Cloned repo" >> $LOGFILE
 
-# Build entire project (runs mvn install on root)
-cd /home/ec2-user/sample.daytrader7 || exit 1
-sudo -u ec2-user mvn install >> $LOGFILE 2>&1
+echo "Running mvn install"
+cd sample.daytrader7
+mvn install
 if [ $? -ne 0 ]; then
-  echo "Maven install failed!" >> $LOGFILE
+  echo "Maven install failed!"
   exit 1
 fi
-echo "Maven install succeeded" >> $LOGFILE
 
-# Kill any running Liberty server
-sudo -u ec2-user pkill -f "mvn liberty:run" || true
-echo "Killed any existing Liberty server" >> $LOGFILE
+echo "Changing directory to daytrader-ee7 and starting Liberty server"
+cd daytrader-ee7 || { echo "Failed to cd to daytrader-ee7"; exit 1; }
+mvn liberty:run > $LIBERTYLOG 2>&1 &
 
-# Start Liberty server in daytrader-ee7 folder
-cd /home/ec2-user/sample.daytrader7/daytrader-ee7 || exit 1
-sudo -u ec2-user nohup mvn liberty:run > $LIBERTYLOG 2>&1 &
-echo "Liberty server started" >> $LOGFILE
+echo "Liberty server started, logs at $LIBERTYLOG"
